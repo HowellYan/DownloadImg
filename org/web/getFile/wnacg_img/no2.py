9b273e45348c4085f2fbc3e41926d0e91f63a81d
HowellYang <1726474652@qq.com>
2018-07-29 21:26:41 +0800

    01

--- org/web/getFile/wnacg_img/no2.py
+++ org/web/getFile/wnacg_img/no2.py
@@ -1,10 +1,37 @@
-# -*-coding:utf-8-*-
-
+# # encoding:utf-8
+import requests
 import os
+import time
 import uuid
 import urllib2
 import cookielib
 import re
+
+def get_local_file_exists_size(local_path):
+    try:
+        lsize = os.stat(local_path).st_size
+    except:
+        lsize = 0
+    return lsize
+
+
+def get_file_obj(down_link, offset):
+    webPage = None
+    try:
+        headers = {'Range': 'bytes=%d-' % offset}
+        webPage = requests.get(down_link, stream=True, headers=headers, timeout=120, verify=False)
+        status_code = webPage.status_code
+        if status_code in [200, 206]:
+            webPage = webPage
+        elif status_code == 416:
+            print u"%s文件数据请求区间错误,status_code:%s" % (down_link, status_code)
+        else:
+            print u"%s链接有误,status_code:%s" % (down_link, status_code)
+    except Exception as e:
+        print u"无法链接:%s,e:%s" % (down_link, e)
+    finally:
+        return webPage
+
 
 #获取文件后缀名
 def get_file_extension(file):
@@ -28,6 +55,32 @@ def mkdir(path):
 def unique_str():
     return str(uuid.uuid1())
 
+
+# down_link = 'http://wnacg.download/down/0001/67dd670f043183f0813716dd5f071089.zip'  # 下载链接
+# file_size = 271768736  # 文件总大小
+# local_path = "d:/imgscloud/1.zip"
+
+def download_file(down_link, file_size, local_path):
+    while True:
+        lsize = get_local_file_exists_size(local_path)
+        if lsize == file_size:
+            break
+        webPage = get_file_obj(down_link, lsize)
+        try:
+            file_obj = open(local_path, 'ab+')
+        except Exception as e:
+            print u"打开文件:%s失败" % local_path
+            break
+        try:
+            for chunk in webPage.iter_content(chunk_size=64 * 1024):
+                if chunk:
+                    file_obj.write(chunk)
+                else:
+                    break
+        except Exception as e:
+            time.sleep(5)
+        file_obj.close()
+        webPage.close()
 
 #'''抓取网页文件内容，保存到内存@url 欲抓取文件 ，path+filename'''
 def get_file(url, fileCount):
@@ -56,6 +109,7 @@ def get_file(url, fileCount):
         file_size = int(meta.getheaders("Content-Length")[0])
         content_type = meta.getheaders('Content-Type')[0].split(';')[0]
         print file_size, content_type
+        download_file(url, file_size, "d:/imgscloud/"+str(fileCount)+".zip")
         # data = f.read()
         # with open("d:/imgscloud/"+str(fileCount)+".zip", "wb") as code:
         #     code.write(data)
@@ -88,13 +142,13 @@ def save_file(path, file_name, data):
 
 
 # 获取文件后缀名
-print get_file_extension("123.jpg")
+# print get_file_extension("123.jpg")
 
 # 創建文件目录，并返回该目录
 # print mkdir("d:/ljq")
 
 # 自动生成一个唯一的字符串，固定长度为36
-print unique_str()
+# print unique_str()
 
 # 7370
 fileCount=1
